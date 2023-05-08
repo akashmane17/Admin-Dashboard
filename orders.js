1d16d2bc604df687bdae7a9237a689f2cca4a8ad
akash <akashmane277@gmail.com>
2023-05-08 22:45:37 +0530

    added dark mode, and some functionality using js

---
+++ orders.js
@@ -0,0 +1,38 @@
+const orders = [
+  {
+    productName: 'Ruko Fu Pro Drone',
+    productNumber: '49346',
+    paymentStatus: 'Due',
+    shipping: 'Pending'
+  },
+  {
+    productName: 'Drone with camera',
+    productNumber: '97678',
+    paymentStatus: 'Paid',
+    shipping: 'Delivered'
+  },
+  {
+    productName: 'Aatrawx Drone',
+    productNumber: '08171',
+    paymentStatus: 'Refunded',
+    shipping: 'Declined'
+  },
+  {
+    productName: 'GPS 4k Drone',
+    productNumber: '54356',
+    paymentStatus: 'Paid',
+    shipping: 'Delivered'
+  },
+  {
+    productName: 'DII Air 2S',
+    productNumber: '90667',
+    paymentStatus: 'due',
+    shipping: 'Pending'
+  },
+  {
+    productName: 'Aatrawx Drone',
+    productNumber: '08171',
+    paymentStatus: 'Paid',
+    shipping: 'Delivered'
+  }
+];
